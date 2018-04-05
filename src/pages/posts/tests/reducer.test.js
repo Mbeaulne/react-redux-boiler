681960336ec35c6135f949b7ba87d3a35bb2805f
mbeaulne <matt.beaulne@gmail.com>
2018-04-05 11:56:46 -0400

    Tests now run and pass

--- src/pages/posts/tests/reducer.test.js
+++ src/pages/posts/tests/reducer.test.js
@@ -1,34 +1,38 @@
-import * as actions from '../actions'
-import reducer from '../reducer'
+/**
+ * @jest-environment node
+ */
+
+import * as actions from '../actions';
+import reducer from '../reducer';
 
 describe('Posts actions and reducer', () => {
   const newPost = {
     title: 'Javascripts 101',
     body: 'Javascript is not like java?'
-  }
+  };
 
   it('runs addPosts action', () => {
-    const actionCalled = actions.addPosts(newPost)
-    expect(actionCalled.type).toEqual('ADD_POSTS')
-    expect(actionCalled.payload).toEqual(newPost)
-  })
+    const actionCalled = actions.addPosts(newPost);
+    expect(actionCalled.type).toEqual('ADD_POSTS');
+    expect(actionCalled.payload).toEqual(newPost);
+  });
 
   it('runs reducer with no action', () => {
     expect(reducer(undefined, {})).toEqual([
       {
-        'body': 'how are things',
-        'title': 'hey dudes'
+        body: 'how are things',
+        title: 'hey dudes'
       }
-    ])
-  })
+    ]);
+  });
 
   it('runs reducer with newPost', () => {
     expect(reducer(undefined, actions.addPosts(newPost))).toEqual([
       {
-        'body': 'how are things',
-        'title': 'hey dudes'
+        body: 'how are things',
+        title: 'hey dudes'
       },
       newPost
-    ])
-  })
-})
+    ]);
+  });
+});
